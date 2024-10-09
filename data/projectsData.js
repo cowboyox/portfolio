c5c010f5bc495a7dfa9cc7c9042f6d6c7956ffc3
cowboyox <cowboyoxeth@gmail.com>
2024-10-10 08:36:38 +0900

    added more projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -7,7 +7,7 @@ export const projectsData = [
     repoUrl: 'https://github.com/cowboyox/Midas-Capital',
     location: 'Hamburg, Germany',
     img: '/portfolio/images/midas (1).png',
-    duration: 'Feb 2021 - 2024 Mar',
+    duration: 'Feb 2021 - Nov 2023',
     tags: 'UI/UX',
     shortTags: 'Next.js + Web3',
     images: [
@@ -49,7 +49,237 @@ export const projectsData = [
       {
         id: uuidv4(),
         details:
-          'Implemented responsive and user-friendly web application. Integrated with Ethereum blockchain using Ethers.js library enabling interaction with smart contracts, wallet connectivity, and transaction handling.',
+          'Implemented responsive and user-friendly DeFi Platform. Integrated with Ethereum blockchain using Ethers.js library enabling interaction with smart contracts, wallet connectivity, and transaction handling.',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Tectum',
+    repoUrl: 'https://github.com/cowboyox/jobs3-tectum',
+    location: 'London, UK',
+    img: '/portfolio/images/tectum (1).png',
+    duration: 'Dec 2023 - Jul 2024',
+    tags: 'UI/UX',
+    shortTags: 'Next.js + Web3',
+    images: [
+      {
+        id: uuidv4(),
+        title: 'Web3 Career Platform',
+        img: '/portfolio/images/tectum (1).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'Web3 Career Platform',
+        img: '/portfolio/images/tectum (2).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'Web3 Career Platform',
+        img: '/portfolio/images/tectum (3).png',
+      },
+    ],
+    tech: [
+      {
+        title: 'Tools & Technologies',
+        techs: [
+          'React.js',
+          'Next.js',
+          'Wagmi',
+          'web3.js',
+          'ethers.js',
+          'Viem',
+          'TypeScript',
+          'Tanstack Query',
+          'TailWindCSS',
+          'solana',
+        ],
+      },
+    ],
+    description: [
+      {
+        id: uuidv4(),
+        details:
+          'Integrated smart contracts to Next.js UI for admin, client, freelancer logics with EVM & SVM. Developed job site like Upwork, Fivrr by using THREE token and other stable coins like USDT, USDC.',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Ionic Money',
+    repoUrl: 'https://github.com/cowboyox/Ionic-Money',
+    location: 'Hamburg, Germany',
+    img: '/portfolio/images/ionic (1).png',
+    duration: 'Feb 2022 - Nov 2023',
+    tags: 'UI/UX',
+    shortTags: 'Next.js + Web3',
+    images: [
+      {
+        id: uuidv4(),
+        title: 'DeFi Platform',
+        img: '/portfolio/images/ionic (1).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'DeFi Platform',
+        img: '/portfolio/images/ionic (2).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'DeFi Platform',
+        img: '/portfolio/images/ionic (3).png',
+      },
+    ],
+    tech: [
+      {
+        title: 'Tools & Technologies',
+        techs: [
+          'React.js',
+          'Next.js',
+          'Node.js',
+          'Wagmi',
+          'web3.js',
+          'ethers.js',
+          'Viem',
+          'TypeScript',
+          'Tanstack Query',
+          'TailWindCSS',
+          'Chakra UI',
+        ],
+      },
+    ],
+    description: [
+      {
+        id: uuidv4(),
+        details:
+          'Implemented responsive and user-friendly DeFi Platform. Integrated with Ethereum blockchain using Ethers.js library enabling interaction with smart contracts, wallet connectivity, and transaction handling. Cloned from Midas Capital',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Rubic Exchange',
+    repoUrl: 'https://github.com/cowboyox/rubic-app',
+    location: 'California, United States',
+    img: '/portfolio/images/rubic (1).png',
+    duration: 'Oct 2019 - Apr 2021',
+    tags: 'UI/UX',
+    shortTags: 'Angular + Web3',
+    images: [
+      {
+        id: uuidv4(),
+        title: 'Exchange Platform',
+        img: '/portfolio/images/rubic (1).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'Exchange Platform',
+        img: '/portfolio/images/rubic (2).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'Exchange Platform',
+        img: '/portfolio/images/rubic (3).png',
+      },
+    ],
+    tech: [
+      {
+        title: 'Tools & Technologies',
+        techs: ['Angular', 'rxjs', 'web3.js', 'ethers.js', 'axios', 'TypeScript', 'TailWindCSS'],
+      },
+    ],
+    description: [
+      {
+        id: uuidv4(),
+        details:
+          'Implemented responsive and user-friendly Crypto Exchange application. Integrated with several blockchain networks such as EVM, Solana using Ethers.js library enabling interaction with smart contracts, wallet connectivity, and transaction handling. Some swaps are confirmed from API.',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Juice Box',
+    repoUrl: 'https://github.com/cowboyox/juice-interface',
+    location: 'London, United Kingdom',
+    img: '/portfolio/images/juice (1).png',
+    duration: 'Jan 2017 - Dec 2018',
+    tags: 'UI/UX',
+    shortTags: 'Next.js + Web3',
+    images: [
+      {
+        id: uuidv4(),
+        title: 'DAO Platform',
+        img: '/portfolio/images/juice (1).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'DAO Platform',
+        img: '/portfolio/images/juice (2).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'DAO Platform',
+        img: '/portfolio/images/juice (3).png',
+      },
+    ],
+    tech: [
+      {
+        title: 'Tools & Technologies',
+        techs: [
+          'Next.js',
+          'React.js',
+          'Wagmi',
+          'Viem',
+          'web3.js',
+          'ethers.js',
+          'axios',
+          'TypeScript',
+          'TailwindCSS',
+          'Supabase',
+          'Antd',
+          'graphQL',
+        ],
+      },
+    ],
+    description: [
+      {
+        id: uuidv4(),
+        details:
+          'Developed scalable and performant web applications. Collaborated with the team to design and implement frontend architecture and application structure. Implemented reusable and modular components to ensure code maintainability and facilitate future development. Implemented asynchronous operations and handled API calls using Redux middleware. Designed and implemented RESTful APIs for various features, including user authentication, product catalog, and order management.',
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: 'Block Tracker',
+    repoUrl: 'https://github.com/cowboyox/block-tracker',
+    location: 'Manila, Philippines',
+    img: '/portfolio/images/block (1).png',
+    duration: 'Apr 2015 - Oct 2016',
+    tags: 'UI/UX',
+    shortTags: 'React.js + Web3',
+    images: [
+      {
+        id: uuidv4(),
+        title: 'DAO Platform',
+        img: '/portfolio/images/block (1).png',
+      },
+      {
+        id: uuidv4(),
+        title: 'DAO Platform',
+        img: '/portfolio/images/block (2).png',
+      },
+    ],
+    tech: [
+      {
+        title: 'Tools & Technologies',
+        techs: ['React.js', 'web3.js', 'ethers.js', 'axios', 'TypeScript', 'Storybook'],
+      },
+    ],
+    description: [
+      {
+        id: uuidv4(),
+        details: 'Block Tracker Application with React.js + Web3.js',
       },
     ],
   },
